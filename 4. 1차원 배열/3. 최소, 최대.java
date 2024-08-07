49161ff296f50f091f2a390a0c511ea26d7c9e19
HyunjunYoo <155802246+HyunjunYoo@users.noreply.github.com>
2024-08-07 15:51:39 +0900

    3. 최소, 최대.java

--- 4. 1차원 배열/3. 최소, 최대.java
+++ 4. 1차원 배열/3. 최소, 최대.java
@@ -1,23 +1,32 @@
 package algorithm_baekjoon;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-		Scanner in = new Scanner(System.in);
-        
-		int N = in.nextInt();
-		int[] arr = new int[N];
-        
-		for (int i = 0; i < N; i++) {
-			arr[i] = in.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] n = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			n[i] = sc.nextInt();
 		}
-        
-		in.close();
-		Arrays.sort(arr);
-		System.out.print(arr[0] + " " + arr[N - 1]);
+		
+		int min = n[0];
+		int max = n[0];
+		
+		for(int i=1; i<n.length; i++) {
+			if((min > n[i])) {
+				min = n[i];
+			}if(max < n[i]) {
+				max = n[i];
+			}
+		}
+
+		System.out.println(min + " " + max);
+		
+		sc.close();
+
 	}
 }
